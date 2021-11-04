d64a6af47b8397060c7dfe2aaf7e385a6944ecc5
Danielle <danielle.alamilla@gmail.com>
2021-11-03 20:42:51 -0400

    submit comment button added to blog

---
+++ comment-button.js
@@ -0,0 +1,5 @@
+const commentButton = document.querySelector('button');
+
+commentButton.addEventListener('click', function () {
+    alert('Comment submitted! Please wait for moderator approval.')
+});
